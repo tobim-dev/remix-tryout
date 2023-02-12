ff3a3a59823ce794a5fac6f21e2ac4db72991108
tobim-dev <mail@tmeyer.me>
2023-02-12 21:55:30 +0700

    test: add test for isDigit function

--- app/utils.test.ts
+++ app/utils.test.ts
@@ -1,4 +1,4 @@
-import { validateEmail } from "./utils";
+import { isDigit, validateEmail } from "./utils";
 
 test("validateEmail returns false for non-emails", () => {
   expect(validateEmail(undefined)).toBe(false);
@@ -11,3 +11,15 @@ test("validateEmail returns false for non-emails", () => {
 test("validateEmail returns true for emails", () => {
   expect(validateEmail("kody@example.com")).toBe(true);
 });
+
+test("isDigit returns true for numbers", () => {
+  expect(isDigit(1 as unknown as string)).toBe(true);
+})
+
+test("isDigit returns false for strings", () => {
+  expect(isDigit("a")).toBe(false);
+})
+
+test("isDigit returns false if more than one character is provided", () => {
+  expect(isDigit(12 as unknown as string)).toBe(false)
+})
